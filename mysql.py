f863800d18f9057cd99f29b146221ccf96fa04d0
Billy SU <g4691821@gmail.com>
2016-12-29 12:53:54 +0800

    fix mysql bug

--- mysql.py
+++ mysql.py
@@ -1,19 +1,20 @@
 import pymysql
 
-class mysql:
-    def connect():
+class MySQL:
+    def connect(self):
         with open("mysql.txt","r") as file:
             host = file.readline().rstrip('\n')
             user = file.readline().rstrip('\n')
             passwd = file.readline().rstrip('\n')
             dbname = file.readline().rstrip('\n')
         try:
-            self.db = pymysql.connect(host,ueser,passwd,dbname,use_unicode=True, charset="utf8")
+            self.db = pymysql.connect(host,user,passwd,dbname,use_unicode=True, charset="utf8")
             self.cursor = self.db.cursor()
         except:
             return 1
         return 0
-    def close():
+    def close(self):
         self.db.close()
 
-
+    def __init__(self):
+        self.connect()
