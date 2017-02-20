bc5f0489f77af837d47aed81078f3bb8dba6f9c5
icecat <icecat2012@gmail.com>
2017-02-20 14:07:55 +0800

    update supervise

--- supervise/supervise.py
+++ supervise/supervise.py
@@ -16,13 +16,16 @@ def main():
 
 	#initial
 	with open(error_dir_txt, 'r') as file_pointer :
-		last_line_count = sum(1 for line_content in file_pointer)
+		for line_content in file_pointer:
+			last_line_count += 1
 	
 
 	while shutdown == 0:
 		sleep(300)
-		file_pointer = open(error_dir_txt, "r")
-		line_count = sum(1 for line_content in file_pointer)
+		line_count = 0
+		with open(error_dir_txt, 'r') as file_pointer :
+			for line_content in file_pointer:
+				line_count += 1
 		msg_content = ""
 		if last_line_count < line_count:
 			if last_line_count + small_bug >= line_count:
@@ -32,8 +35,8 @@ def main():
 			elif last_line_count + big_bug >= line_count:
 				msg_content = ("arrange_schedule got "+ str(big_bug) +" error last 5 minute \r\n")
 			else :
-				msg_content = ("arrange_schedule got more than "+ str(big_bug) +" error last 5 minute \r\n")
-				msg_content = (msg_content + "arrange_schedule ceased \r\n")
+				msg_content = ("arrange_schedule got more than "+ str(big_bug) +" error last 5 minute. \r\n")
+				msg_content = (msg_content + "arrange_schedule ceased. \r\n")
 				shutdown = 1
 				content = ""
 				with open(setting_dir_txt, 'r') as file_pointer2 :
@@ -45,7 +48,6 @@ def main():
 					file_pointer2.write(content)
 			sendgmail(['FROM_BOT'],['icecat2012@gmail.com'], [''], [''], 'supervise', msg_content)
 		last_line_count = line_count
-		file_pointer.close()
 	sleep(3)
 	return 1
 
