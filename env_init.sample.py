798f814da84ee9d1a694a21e0bb40086548f8e0b
chenyang14 <alexli841114@gmail.com>
2017-02-23 00:46:04 +0800

    add exception handle

--- env_init.sample.py
+++ env_init.sample.py
@@ -3,14 +3,17 @@ import subprocess
 import bcrypt
 import os
 
-with open("mysql_auth.txt","r") as fp:
-    host = fp.readline().rstrip()
-    user = fp.readline().rstrip()
-    passwd = fp.readline().rstrip()
-    dbname = fp.readline().rstrip()
+try:
+    with open("mysql_auth.txt","r") as fp:
+        host = fp.readline().rstrip()
+        user = fp.readline().rstrip()
+        passwd = fp.readline().rstrip()
+        dbname = fp.readline().rstrip()
+except:
+    print("Open authorization file failed")
+    exit(1)
 
-
-print("Connect to MySQL server")
+print("Connect to MySQL server...")
 try:
     client = pymysql.connect(host,user,passwd,use_unicode=True,charset="utf8")
     cursor = client.cursor()
@@ -18,26 +21,39 @@ except:
     print("Connect failed")
     exit(1)
 
-print("Dropping database \"%s\" if existed" % dbname)
+print("Dropping database \"%s\" if existed..." % dbname)
 cursor.execute("DROP DATABASE IF EXISTS "+dbname)
-print("Creating database \"%s\"" % dbname)
+print("Creating database \"%s\"..." % dbname)
 cursor.execute("CREATE DATABASE %s character set utf8" % dbname)
-
-with open("create_tables.sql","r") as file:
-    subprocess.call(["mysql","-u",user,"-p"+passwd],stdin=file)
-
+try:
+    with open("create_tables1.sql","r") as file:
+        subprocess.call(["mysql","-u",user,"-p"+passwd],stdin=file)
+except:
+    print("Open sql file failed")
+    exit(1)
 client.close()
 
-client = pymysql.connect(host,user,passwd,dbname,use_unicode=True,charset='utf8')
-cursor = client.cursor()
+try:
+    client = pymysql.connect(host,user,passwd,dbname,use_unicode=True,charset='utf8')
+    cursor = client.cursor()
+except:
+    print("Connect failed")
+    exit(1)
 
-print("create user \"admin\"")
+print("create user \"admin\"...")
 user_name = 'admin'
 user_hashed_passwd = bcrypt.hashpw('admin'.encode('utf-8'),bcrypt.gensalt())
-ret = cursor.execute("insert into `user` (`user_name`,`user_password`) values (%s,%s)", (user_name,user_hashed_passwd))
+try:
+    ret = cursor.execute("insert into `user` (`user_name`,`user_password`) values (%s,%s)", (user_name,user_hashed_passwd))
+except:
+    print("Insert user failed")
 
-print("create data_type \"圖片\"")
-ret = cursor.execute('insert into `data_type` (`type_name`,`type_dir`) values ("圖片","圖片/")')
+print("create data_type \"圖片\"...")
+try:
+    ret = cursor.execute('insert into `data_type` (`type_name`,`type_dir`) values ("圖片","圖片/")')
+except:
+    print("Insert type failed")
+
 if not os.path.exists("static/圖片"):
     print("create dir \"圖片\"")
     os.makedirs("static/圖片")
