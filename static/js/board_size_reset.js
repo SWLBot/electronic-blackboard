4e70bab406b01306b362ac7fa74d9e04657b5556
Billy Su <g4691821@gmail.com>
2017-04-07 22:48:32 +0800

    Move board size reset to js file

---
+++ static/js/board_size_reset.js
@@ -0,0 +1,8 @@
+$('div.container').height(screen.height * 0.98);
+$('div.container').width(screen.width * 0.9);
+$('div.header').height(screen.height * 0.35);
+$('div.header').width(screen.width * 0.9);
+$('div.content').height(screen.height * 0.63);
+$('div.content').width(screen.width * 0.9);
+$('div.bar').height(screen.height * 0.01);
+$('div.bar').width(screen.width * 0.9);
