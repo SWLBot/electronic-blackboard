286e732805e6d08ff499a423f614ef55c44f0800
Billy Su <g4691821@gmail.com>
2017-06-23 21:46:24 +0800

    Add test_server_api to main test

--- test/test.py
+++ test/test.py
@@ -1,10 +1,11 @@
 from test_mysql import Mysql
 from test_env_init import Env_init
 from test_arrange_schedule import Arrange_Schedule
+from test_server_api import Server_api
 import unittest
 
 def main():
-    testcases = [Mysql,Env_init,Arrange_Schedule]
+    testcases = [Mysql,Env_init,Arrange_Schedule,Server_api]
     suite = unittest.TestSuite()
     for case in testcases:
         tmp = unittest.TestLoader().loadTestsFromTestCase(case)
