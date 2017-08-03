aff2726d765cdec90451a558af36bc1d62871ffd
chenyang14 <alexli841114@gmail.com>
2017-08-03 17:07:24 +0800

    Add cleanSchedule method by scheSn

--- dataAccessObjects.py
+++ dataAccessObjects.py
@@ -152,8 +152,11 @@ class ScheduleDao(DefaultDao):
             +" WHERE sche_sn={scheSn}".format(scheSn=scheSn)
         self.db.cmd(sql)
 
-    def cleanSchedule(self):
-        sql = 'DELETE FROM schedule WHERE sche_is_used=0'
+    def cleanSchedule(self,scheSn=None):
+        if scheSn:
+            sql = "DELETE FROM schedule WHERE sche_sn={scheSn}".format(scheSn=scheSn)
+        else:
+            sql = 'DELETE FROM schedule WHERE sche_is_used=0'
         self.db.cmd(sql)
 
     def countUsedSchedule(self):
