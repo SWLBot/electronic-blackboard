8c21804fe9f17a6805b818a8bc1542110dca9c76
Billy Su <g4691821@gmail.com>
2017-08-27 15:54:39 +0800

    Add several google calendar ids
    Let google calendar worker grabs several calendar id's
    events, such that user can add customized activity,
    instead of only grabbing NCTU calendar events.

--- server_api.py
+++ server_api.py
@@ -1253,11 +1253,21 @@ def exchange_code_and_store_credentials(code):
     return credentials
 
 def get_upcoming_events(credentials):
+    target_calendars = ['nctupac@gmail.com']
+    events = []
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    eventsResult = service.events().list(calendarId='nctupac@gmail.com',maxResults=10,timeMin=now).execute()
-    return eventsResult
+    try:
+        calendars = service.calendarList().list().execute()['items']
+        for calendar in calendars:
+            target_calendars.append(calendar['id'])
+    except Exception as e:
+        print(str(e))
+    for calendarId in target_calendars:
+        now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+        eventsResult = service.events().list(calendarId=calendarId,maxResults=10,timeMin=now).execute()
+        events.extend(eventsResult['items'])
+    return events
 
 #crawler handle
 def news_insert_db(json_obj):
