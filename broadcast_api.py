af653abb12a735f6781122dc63a848d0f17aa014
Billy Su <g4691821@gmail.com>
2017-12-16 13:56:12 +0800

    Refactor load_schedule() for readability

--- broadcast_api.py
+++ broadcast_api.py
@@ -7,15 +7,56 @@ from dataAccessObjects import *
 import os.path
 import json
 
+def getDisplayContent(sche_target_id,return_msg):
+    #get type from target id prefix
+    targetIdPrefix = sche_target_id[:4]
+    if targetIdPrefix == "imge":
+        with ImageDao() as imageDao:
+            file_info = imageDao.getIdSysName(Id=sche_target_id)
+        return_msg["file_type"] = "image"
+    elif targetIdPrefix == "text":
+        with TextDao() as textDao:
+            file_info = textDao.getIdSysName(Id=sche_target_id)
+        return_msg["file_type"] = "text"
+    else :
+        return_msg["error"] = "target id type error {}".format(targetIdPrefix)
+        return return_msg
+
+    try:
+        type_id = file_info['typeId']
+        system_file_name = file_info['systemName']
+        return_msg["like_count"] = file_info['likeCount']
+    except:
+        return_msg["error"] = "no file record"
+        return return_msg
+
+    with DataTypeDao() as dataTypeDao:
+        type_dir = dataTypeDao.getTypeDir(typeId=type_id)
+        type_name = dataTypeDao.getTypeName(typeId=type_id)
+    if type_dir == None or type_name == None:
+        return_msg["error"] = "No such type id {}".format(type_id)
+        return return_msg
+
+    targetFile = os.path.join("static", type_dir, system_file_name)
+    return_msg["file"] = os.path.join(type_dir, system_file_name)
+    return_msg["type_name"] = type_name
+
+    #if text read file
+    if return_msg["file_type"] == "text":
+        if not os.path.isfile(targetFile) :
+            return_msg["error"] = "no file"
+            return return_msg
+        else :
+            with open(targetFile,"r") as fp:
+                file_content = json.load(fp)
+            return_msg["file_text"] = file_content
+    return return_msg
+
 #The API load schedule.txt and find out the first image which has not print and the time limit still allow
 def load_schedule():
     try:
         return_msg = {}
         return_msg["result"] = "fail"
-        schedule_dir = ""
-        sche_target_id = ""
-        type_id = ""
-        system_file_name = ""
 
         #find next schedule
         with ScheduleDao() as scheduleDao:
@@ -27,47 +68,9 @@ def load_schedule():
         sche_target_id = next_schedule['sche_target_id']
         return_msg["display_time"] = int(next_schedule['display_time'])
 
-        #find the file
-        if sche_target_id[0:4]=="imge":
-            with ImageDao() as imageDao:
-                file_info = imageDao.getIdSysName(Id=sche_target_id)
-            return_msg["file_type"] = "image" 
-        elif sche_target_id[0:4]=="text":
-            with TextDao() as textDao:
-                file_info = textDao.getIdSysName(Id=sche_target_id)
-            return_msg["file_type"] = "text"
-        else :
-            return_msg["error"] = "target id type error"
+        return_msg = getDisplayContent(sche_target_id,return_msg)
+        if "error" in return_msg:
             return return_msg
-        try:
-            type_id = int(file_info['typeId'])
-            system_file_name = file_info['systemName']
-            return_msg["like_count"] = int(file_info['likeCount'])
-        except:
-            return_msg["error"] = "no file record"
-            return return_msg
-
-        #find type dir
-        with DataTypeDao() as dataTypeDao:
-            type_dir = dataTypeDao.getTypeDir(typeId=type_id)
-            type_name = dataTypeDao.getTypeName(typeId=type_id)
-        try:
-            schedule_dir = os.path.join(schedule_dir, "static", type_dir, system_file_name)
-            return_msg["file"] = os.path.join(type_dir, system_file_name)
-            return_msg["type_name"] = str(type_name)
-        except:
-            return_msg["error"] = "no type record"
-            return return_msg
-
-        #if text read file
-        if return_msg["file_type"] == "text":
-            if not os.path.isfile(schedule_dir) :
-                return_msg["error"] = "no file"
-                return return_msg
-            else :
-                with open(schedule_dir,"r") as fp:
-                    file_content = json.load(fp)
-                return_msg["file_text"] = file_content
 
         #update display count
         if return_msg["file_type"] == "image":
