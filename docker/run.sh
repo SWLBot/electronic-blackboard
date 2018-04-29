584988fbcf66742a80bb58639788971f0a286caa
Dennis Chung <stvreumi@livemail.tw>
2018-04-29 22:29:37 +0800

    modify run.sh information

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,7 @@
 unameOut="$(uname -s)"
 sudo docker run -d --name=EB-mysql --env=MYSQL_ROOT_PASSWORD=mypassword mysql:5.7
 case "${unameOut}" in
-    Linux*)  echo -e "EB-mysql\nroot\nmypassword\nblackboard" > mysql_auth.txt;;
+    Linux*)  echo "EB-mysql\nroot\nmypassword\nblackboard" > mysql_auth.txt;;
     Darwin*) echo "EB-mysql\nroot\nmypassword\nblackboard" > mysql_auth.txt;;
     *)       echo "unknown machine";;
 esac
